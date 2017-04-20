cd7256fb131949e5642208d1e6f790b0f8013732
Jasonface900 <jorge.floresm900@gmail.com>
2017-04-20 12:44:07 -0500

    testing effects

--- src/launchermain.cpp
+++ src/launchermain.cpp
@@ -7,11 +7,12 @@
 /* 
  * File:   launchermain.cpp
  * Author: Jorge
- *
+ * Currently being used as a testing ground.
  * Created on April 16, 2017, 11:10 AM
  */
 
 #include <cstdlib>
+#include <ctime>
 #include <iostream>
 #include <string>
 #include <map>
@@ -21,15 +22,53 @@
  */
 int main(int argc, char** argv) {
     // MAP CREATION
-    std::map<int, std::pair<int, std::string> > effects;
-    std::map<int, std::pair<int, std::string> >::iterator effectsIt;
+    std::map <int, std::pair <int, std::string> > effects;
+    std::map <int, std::pair <int, std::string> >::iterator effectsIt;
 
+    for(int i = 0; i != 10; i++){
+        std::string e = "NULL";
+	//std::srand(std::time(NULL));
+        switch(std::rand() % 10){
+            case 0:
+                e = "water_cold";
+                break;
+            case 1:
+                e = "fire_magic";
+                break;
+            case 2:
+                e = "blood_losing";
+                break;
+            case 3:
+                e = "fire_lava";
+                break;
+            case 4:
+                e = "ice_magic";
+                break;
+            case 5:
+                e = "poison_animal";
+                break;
+            case 6:
+                e = "water_hot";
+                break;
+            case 7:
+                e = "ice_nature";
+                break;
+            case 8:
+                e = "healing_magic";
+                break;
+            case 9:
+                e = "healing_potion";
+                break;
+        }
+        effects[i] = std::make_pair((std::rand() % 100)/10, e);
+    }
+   /*
     effects[0]= std::make_pair(5, "poison");
     effects[1]= std::make_pair(2, "fire");
     effects[2]= std::make_pair(2, "frozen");
     effects[3]= std::make_pair(6, "heal");
     
-    /*
+    
     effectsIt = effects.find(1);
     if (effectsIt != effects.end())
       effects.erase (effectsIt);
