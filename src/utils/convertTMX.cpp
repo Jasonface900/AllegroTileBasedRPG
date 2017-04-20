a2c7bfd42ad44deddb83efe2b6420f91b1211d3f
Jasonface900 <jorge.floresm900@gmail.com>
2017-04-20 16:57:02 -0500

    idek

--- src/utils/convertTMX.cpp
+++ src/utils/convertTMX.cpp
@@ -1,11 +1,14 @@
 ////TODO: WRITE A TMX LOADER, THAT READS A TMX FILE AND SEPERATES IT INTO 20x15 (300) TILE SECTIONS.
 //// THIS ASSUMES A 32x32 FOR USE WITH A 640x480 RESOLUTION.
 //// DIRECTORY LIST
+
+#
+
 /*
    └── utils
+        ├── convertTMX.cpp
         └── TMXloader
+             └── convertTMX.cpp
         └── convertTMX.cpp
-
-
 */
 
